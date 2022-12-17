0e03e4921562d8bf3e3f43985b764328212b3d4d
Daniel Venev <110028635+divenev@users.noreply.github.com>
2022-12-17 13:45:39 +0200

    Task Manager v7

--- TaskManager/appuser/admin.py
+++ TaskManager/appuser/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 
-from TaskManager.appuser.models import AppUser
-
 UserModel = get_user_model()
 
 
--- TaskManager/appuser/views.py
+++ TaskManager/appuser/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, LogoutView
 
+
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
