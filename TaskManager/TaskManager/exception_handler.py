09d9628d986c5976b80e68b5b7d8285f180613b2
Daniel Venev <110028635+divenev@users.noreply.github.com>
2022-12-17 12:23:26 +0200

    грешно място

--- TaskManager/TaskManager/exception_handler.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-
-def handler_error500(request, *args, **kwargs):
-    return render(request, 'webtask/error_template.html')
-
-def handler_error404(request, *args, **kwargs):
-    return render(request, 'webtask/error_template.html')
--- TaskManager/TaskManager/webtask/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class WebtaskConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'TaskManager.webtask'
